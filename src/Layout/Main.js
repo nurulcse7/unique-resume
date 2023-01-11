52cf2bf538a32cf86fee9c9895111791e007fc38
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-11 20:47:34 +0600

    footer added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../common/Footer/Footer";
 import Navbar from "../common/Navbar/Navbar";
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };

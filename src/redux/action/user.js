29c354ddf414b5ad196e017cdfce322e0c44c925
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-19 18:53:56 +0600

    problem fix checking

--- src/redux/action/user.js
+++ src/redux/action/user.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { server } from "../store";
 
 export const login = (userInfo) => async (dispatch) => {
   try {
     dispatch({ type: "loginRequest" });
-    const { data } = await axios.post(`/api/login`, userInfo, {
+    const { data } = await axios.post(`${server}/api/login`, userInfo, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -19,7 +20,7 @@ export const login = (userInfo) => async (dispatch) => {
 export const register = (userInfo) => async (dispatch) => {
   try {
     dispatch({ type: "registerRequest" });
-    const { data } = await axios.post(`/api/register`, userInfo, {
+    const { data } = await axios.post(`${server}/api/register`, userInfo, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -34,7 +35,7 @@ export const register = (userInfo) => async (dispatch) => {
 export const signOut = () => async (dispatch) => {
   try {
     dispatch({ type: "logoutRequest" });
-    const { data } = await axios.get(`/api/logout`, {
+    const { data } = await axios.get(`${server}/api/logout`, {
       withCredentials: true,
     });
 
@@ -46,7 +47,7 @@ export const signOut = () => async (dispatch) => {
 export const getMyProfile = () => async (dispatch) => {
   try {
     dispatch({ type: "loadUserRequest" });
-    const { data } = await axios.get(`/api/user-details`, {
+    const { data } = await axios.get(`${server}/api/user-details`, {
       withCredentials: true,
     });
 
@@ -58,12 +59,16 @@ export const getMyProfile = () => async (dispatch) => {
 export const forgotPass = (userInfo) => async (dispatch) => {
   try {
     dispatch({ type: "forgotPassRequest" });
-    const { data } = await axios.post(`api/password/forgot`, userInfo, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-    });
+    const { data } = await axios.post(
+      `${server}/api/password/forgot`,
+      userInfo,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      }
+    );
 
     dispatch({ type: "forgotPassSuccess", payload: data });
   } catch (error) {
@@ -74,7 +79,7 @@ export const forgotPass = (userInfo) => async (dispatch) => {
 export const userData = () => async (dispatch) => {
   try {
     dispatch({ type: "forgotPassRequest" });
-    const { data } = await axios.get(`api/allusers`, {
+    const { data } = await axios.get(`${server}/api/allusers`, {
       withCredentials: true,
     });
 

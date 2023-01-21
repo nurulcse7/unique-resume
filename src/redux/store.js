9c9e1bdb836d1d1d8e08504a6c3182fca51f1c67
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-22 00:42:00 +0600

    all almost done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { loadUser } from "./reducers/loadUser";
 import { userReducer } from "./reducers/userReducer";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    userData: loadUser,
   },
 });
 

3b82e49ea9227e57f7f112042ac7908917f23287
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-24 20:46:04 +0600

    register page problem solved

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://unique-resume.onrender.com",
 });
 
 export default axiosInstance;

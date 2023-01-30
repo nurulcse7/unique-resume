ce9f0e2d638da4ccac4e6ac1449932ffb44253ff
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-30 13:49:01 +0600

    editor page change

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://unique-resumeserver.vercel.app",
 });
 
 export default axiosInstance;

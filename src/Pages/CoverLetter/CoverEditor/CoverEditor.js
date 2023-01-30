230dd1758f65775d619209e8e1316dd8fb3970ca
coderrabbie <golamrabbi.dv@gmail.com>
2023-01-30 17:25:32 +0600

    cover letter page design fix

--- src/Pages/CoverLetter/CoverEditor/CoverEditor.js
+++ src/Pages/CoverLetter/CoverEditor/CoverEditor.js
@@ -1,31 +1,82 @@
-import React from "react";
-import {
-  ApartmentOutlined,
-  AuditOutlined,
-  ContactsOutlined,
-  CrownOutlined,
-} from "@ant-design/icons";
+import React, { useRef, useState } from "react";
+
 import PersonalDetails from "../PersonalDetails/PersonalDetails";
 import EmployerDetails from "../EmployerDetails/EmployerDetails";
 import LetterDetails from "../LetterDetails/LetterDetails";
+import { Button, Modal } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import Template1 from "../../ResumeTemplate/Template1";
+import Template2 from "../../ResumeTemplate/Template2";
+import Template3 from "../../ResumeTemplate/Template3";
+import { useReactToPrint } from "react-to-print";
+import styles from "../../../style";
 
 const CoverEditor = () => {
+  const [open, setOpen] = useState(false);
+  const componentRef = useRef();
+  const navigate = useNavigate();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+  const params = useParams();
+  const gettemplate = () => {
+    switch (params.id) {
+      case "1": {
+        return <Template1 />;
+      }
+      case "2": {
+        return <Template2 />;
+      }
+      case "3": {
+        return <Template3 />;
+      }
+      default:
+        return;
+    }
+  };
   return (
     <div>
-      <section className="">
-        <div className="px-20  flex md:flex-row flex-col w-full text-left bg-gray-100   justify-between items-top ">
-          <div className="mb-20  max-w-[500px]  flex-1">
-            <div className="mr-5 mt-20">
+      <section className={`${styles.padding}  bg-gray-100 `}>
+        <div className="w-full text-left  justify-between items-top ">
+          <div className=" ">
+            <div className="">
               <PersonalDetails />
               <EmployerDetails />
               <LetterDetails />
             </div>
           </div>
-          <div className="flex-1 fixed max-h-[800px] p-6 w-[50%] bg-gray-700 flex-nowrap  top-0 bottom-0 right-0 ">
-            <div className="absolute">
-              {/* <div className={`${styles.padding}  `}>{gettemplate()}</div> */}
+          <>
+            <div
+              className="btn-body fixed bottom-0 right-0"
+              onClick={() => setOpen(true)}
+            >
+              <button className="btn btn-hover">
+                <span className="btn-text">Preview & Download</span>
+              </button>
             </div>
-          </div>
+            <Modal
+              centered
+              open={open}
+              onOk={() => setOpen(false)}
+              onCancel={() => setOpen(false)}
+              width={1000}
+            >
+              <div className="flex justify-end my-5 mx-5 px-12 pb-5">
+                <Button
+                  className="back-btn"
+                  onClick={() => navigate("/resume-templates")}
+                >
+                  Back
+                </Button>
+                <Button className="mx-5" onClick={handlePrint}>
+                  Print
+                </Button>
+              </div>
+              <div className="px-12 h-[100%] bg-white" ref={componentRef}>
+                {gettemplate()}
+              </div>
+            </Modal>
+          </>
         </div>
       </section>
     </div>

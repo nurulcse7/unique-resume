90eef9f5124a4c7c1e8c0ea5ac3d80ddd6f9e6fd
Mostafizur Rahman <mostafizur0195@gmail.com>
2023-02-07 13:03:22 +0600

    resume template 6 50% done

--- src/Pages/ResumeTemplate/Template6.js
+++ src/Pages/ResumeTemplate/Template6.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { FaFacebook, FaGithubSquare, FaLink, FaLinkedin } from "react-icons/fa";
 
 const Template6 = ({ data }) => {
   console.log("Template6", data);
+
   return (
     <section className="w-[21cm] m-10 border border-black rounded-md mx-auto text-left relative h-[29.7cm] shadow-2xl">
       <div className="flex w-full h-full ">
@@ -44,40 +46,77 @@ const Template6 = ({ data }) => {
               </div>
 
               <div className="border-t-2 border-gray-600  pt-8 ">
-                <div className="w-full flex items-center flex-col">
-                  <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
+                <div className="w-full flex items-center ">
+                  {/* <div className="w-[25px] h-[25px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
                     <p>
-                      <i class="fa-brands text-2xl fa-linkedin-in"></i>
+                      <i class="fa-brands text-lg fa-linkedin-in"></i>
                     </p>
-                  </div>
-                  <p className="p-2">Likedin Link</p>
+                  </div> */}
+                  <p className="text-3xl">
+                    <FaLinkedin />
+                  </p>
+
+                  <a
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/mostafizur-pro/"
+                    className="p-2"
+                  >
+                    LikedIn Link
+                  </a>
                 </div>
 
-                <div className="w-full flex items-center pt-3 flex-col">
-                  <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
+                <div className="w-full flex items-center pt-3 ">
+                  {/* <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
                     <p>
                       <i class="fa-brands text-3xl fa-github"></i>
                     </p>
-                  </div>
-                  <p className="p-2">GitHub Link</p>
+                  </div> */}
+                  <p className="text-3xl">
+                    <FaGithubSquare />
+                  </p>
+                  <a
+                    rel="noreferrer"
+                    href="https://github.com/Mostafizur"
+                    className="p-2"
+                  >
+                    GitHub Link
+                  </a>
                 </div>
 
-                <div className="w-full flex items-center pt-3 flex-col">
-                  <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
+                <div className="w-full flex items-center pt-3">
+                  {/* <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
                     <p>
                       <i class="fa-solid text-2xl fa-link"></i>
                     </p>
-                  </div>
-                  <p className="p-2">Portfolio Link</p>
+                  </div> */}
+                  <p className="text-3xl">
+                    <FaLink />
+                  </p>
+                  <a
+                    rel="noreferrer"
+                    href="https://mostafizur-rahman-d2b32.web.app/"
+                    className="p-2"
+                  >
+                    Portfolio Link
+                  </a>
                 </div>
 
-                <div className="w-full flex items-center pt-3 flex-col">
-                  <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
+                <div className="w-full flex items-center pt-3 ">
+                  {/* <div className="w-[50px] h-[50px] leading-none rounded-full flex justify-center items-center bg-gray-700 text-white">
                     <p>
                       <i class="fa-brands text-2xl fa-facebook-f"></i>
                     </p>
-                  </div>
-                  <p className="p-2">Facebook Link</p>
+                  </div> */}
+                  <p className="text-3xl">
+                    <FaFacebook />
+                  </p>
+                  <a
+                    rel="noreferrer"
+                    href="https://www.facebook.com/mostafizur.proo/"
+                    className="p-2"
+                  >
+                    Facebook Link
+                  </a>
                 </div>
               </div>
             </div>
@@ -126,96 +165,57 @@ const Template6 = ({ data }) => {
 
               <div>
                 <div className="leading-none pt-3">
-                  <div>
-                    <div className="flex text-black items-center font-semibold text-xl">
-                      <p className="w-[200px]">Graphic Designer</p>
-                      <p>Soft Design Studios</p>
-                    </div>
+                  {data.employmentHistory.map((employ) => (
+                    <>
+                      <div>
+                        <div className="flex text-black items-start  text-base">
+                          <p className="w-[50%]">{employ?.jobTitle}</p>
+                          <p className="w-[50%]">{employ?.employer}</p>
+                        </div>
 
-                    <div className="flex items-center mt-1">
-                      <p className="w-[380px]">2015 - 2017</p>
-                      <small className="w-full text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur isicing elit.
-                        Enim nemo rerum dicta, officia obcaecati molestiae odit
-                        cumque qui unde vitae officiis suscipit eum quos.
-                      </small>
-                    </div>
-                  </div>
-
-                  <div className="mt-2">
-                    <div className="flex text-black items-center font-semibold text-xl">
-                      <p className="w-[200px]">Web Designer </p>
-                      <p>Web Tech TLD</p>
-                    </div>
-
-                    <div className="flex items-center mt-1">
-                      <p className="w-[380px]">2013 - 2015</p>
-                      <small className="w-full text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur isicing elit.
-                        Enim nemo rerum dicta, officia obcaecati molestiae odit
-                        cumque qui unde vitae officiis suscipit eum quos.
-                      </small>
-                    </div>
-                  </div>
-
-                  <div className="mt-2">
-                    <div className="flex text-black items-center font-semibold text-xl">
-                      <p className="w-[200px]">Lead Web Designer </p>
-                      <p>Dev Cewative Solutions</p>
-                    </div>
-
-                    <div className="flex items-center mt-1">
-                      <p className="w-[380px]">2010 - 2013 </p>
-                      <small className="w-full text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur isicing elit.
-                        Enim nemo rerum dicta, officia obcaecati molestiae odit
-                        cumque qui unde vitae officiis suscipit eum quos.
-                        officiis suscipit eum quos.
-                      </small>
-                    </div>
-                  </div>
+                        <div className="flex items-center mt-2">
+                          <p className="w-[50%]">
+                            {employ?.date[0].slice(0, 4)} -
+                            {employ?.date[1].slice(0, 4)}
+                          </p>
+                          <small className="w-[50%] mb-3 text-gray-500 text-justify">
+                            {employ?.description.slice(0, 100) + "..."}
+                          </small>
+                        </div>
+                      </div>
+                    </>
+                  ))}
                 </div>
               </div>
             </div>
 
             <div className="mt-2">
-              <div className="flex py-2  items-center border-b-2 border-gray-300">
-                <p className="text-2xl font-bold uppercase">education </p>
+              <div className="flex items-center border-b-2 border-gray-300">
+                <p className="text-xl font-bold uppercase">Education </p>
               </div>
 
               <div>
                 <div className="leading-none pt-3">
-                  <div>
-                    <div className="flex text-black items-center font-semibold text-xl">
-                      <p className="w-[200px]">University of Lorem</p>
-                      <p>Certicate of Web Trainig</p>
-                    </div>
+                  {data?.education.map((edu) => (
+                    <>
+                      <div>
+                        <div className="flex text-black items-start  text-base">
+                          <p className="w-[50%]">{edu?.school}</p>
+                          <p className="w-[50%]">{edu?.degree}</p>
+                        </div>
 
-                    <div className="flex items-center mt-2">
-                      <p className="w-[380px]">2008 - 2010</p>
-                      <small className="w-full text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur isicing elit.
-                        Enim nemo rerum dicta, officia obcaecati molestiae.
-                        Lorem ipsum dolor sit amet officiis suscipit eum quos.
-                      </small>
-                    </div>
-                  </div>
-
-                  <div className="mt-2">
-                    <div className="flex text-black items-center font-semibold text-xl">
-                      <p className="w-[200px]">University of Lorem </p>
-                      <p>Bechelor of Art Director</p>
-                    </div>
-
-                    <div className="flex items-center mt-1">
-                      <p className="w-[380px]">2007 - 2009</p>
-                      <small className="w-full text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur isicing elit.
-                        Enim nemo rerum dicta, officia obcaecati molestiae.
-                        Lorem ipsum dolor sit amet
-                      </small>
-                    </div>
-                  </div>
+                        <div className="flex items-center mt-2">
+                          <p className="w-[50%]">
+                            {edu?.date[0].slice(0, 4)} -
+                            {edu?.date[1].slice(0, 4)}
+                          </p>
+                          <small className="w-[50%] mb-3 text-gray-500 text-justify">
+                            {edu?.description.slice(0, 100) + "..."}
+                          </small>
+                        </div>
+                      </div>
+                    </>
+                  ))}
                 </div>
               </div>
             </div>

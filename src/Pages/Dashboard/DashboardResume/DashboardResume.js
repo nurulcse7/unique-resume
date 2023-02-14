43a0aa640f2604a1a2bc4b5c5e7ec039055f2b74
ariyanabdullah <ak01859136696@gmail.com>
2023-02-14 17:10:24 +0600

    dashboard updated

--- src/Pages/Dashboard/DashboardResume/DashboardResume.js
+++ src/Pages/Dashboard/DashboardResume/DashboardResume.js
@@ -221,7 +221,10 @@ const DashboardResume = () => {
 
                     <div className={`${ismove ? "  delay-[10s]  " : "hidden"}`}>
                       <div className="absolute w-[600px] h-[241px]  bg-black hover:z-10 justify-center opacity-75 flex top-0 rounded-sm left-0">
-                        <button className="flex items-center space-x-2  ">
+                        <button
+                          onClick={() => handleUpdateResume(template?._id)}
+                          className="flex items-center space-x-2  "
+                        >
                           {" "}
                           <span className=" text-sky-600 text-xl">
                             <FaCompress />{" "}
@@ -270,7 +273,10 @@ const DashboardResume = () => {
                         {" "}
                         <FaCloudDownloadAlt />{" "}
                       </span>
-                      <button className=" text-black  text-sm font-bold  hover:text-sky-500">
+                      <button
+                        onClick={() => handleUpdateResume(template?._id)}
+                        className=" text-black  text-sm font-bold  hover:text-sky-500"
+                      >
                         {" "}
                         Download Resume{" "}
                       </button>

1f9c55c47c215d15c0b3c1f93e0187828cefea34
coderrabbie <golamrabbi.dv@gmail.com>
2023-03-26 22:05:34 +0600

    project updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import CallToAction from '../components/CallToAction/CallToAction';
-import Features from '../components/Features/Features';
-import HowToWork from '../components/HowToWork/HowToWork';
-import Slider from '../components/Slider/Slider';
-import Faq from '../components/FAQ/Faq';
-import HeroSection from '../components/HeroSection';
-import useTitle from '../hooks/useTitle';
+import React from "react";
+import CallToAction from "../components/CallToAction/CallToAction";
+import Features from "../components/Features/Features";
+import HowToWork from "../components/HowToWork/HowToWork";
+import Slider from "../components/Slider/Slider";
+import Faq from "../components/FAQ/Faq";
+import HeroSection from "../components/HeroSection";
+import useTitle from "../hooks/useTitle";
+import FeatureBrands from "../components/FeatureBrands/FeatureBrands";
+import CoverLetterCTA from "../components/CoverLetterCTA/CoverLetterCTA";
 
 const Home = () => {
-  useTitle('Home');
+  useTitle("Home");
   return (
     <div>
       <HeroSection />
       <Slider />
       <HowToWork />
+      <FeatureBrands />
       <CallToAction />
+      <CoverLetterCTA />
       <Features />
       <Faq></Faq>
     </div>
